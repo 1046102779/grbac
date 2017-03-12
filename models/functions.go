1fcb82b8784d8994e466ec5d4c53ad906a0783eb
chendonghai <cdh_cjx@163.com>
2017-03-12 14:06:30 +0800

    优化多叉树的结构

--- models/functions.go
+++ models/functions.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -38,7 +39,8 @@ func (t *Functions) ReadFunctionNoLock(o *orm.Ormer) (retcode int, err error) {
 	defer Logger.Info("[%v] left ReadFunctionNoLock.", t.Id)
 	if o == nil {
 		err = errors.New("param `orm.Ormer` ptr empty")
-		retcode = consts.ERROR_CODE__PARAM__ILLEGAL
+		retcode = consts.ERROR_CODE__SOURCE_DATA__ILLEGAL
+		return
 	}
 	if err = (*o).Read(t); err != nil {
 		err = errors.Wrap(err, "ReadFunctionNoLock")
@@ -53,12 +55,12 @@ func (t *Functions) UpdateFunctionNoLock(o *orm.Ormer) (retcode int, err error) 
 	defer Logger.Info("[%v] left ModifyFunctionNoLock.", t.Id)
 	if o == nil {
 		err = errors.New("param `orm.Ormer` ptr empty")
-		retcode = consts.ERROR_CODE__PARAM__ILLEGAL
+		retcode = consts.ERROR_CODE__SOURCE_DATA__ILLEGAL
 		return
 	}
 	if _, err = (*o).Update(t); err != nil {
 		err = errors.Wrap(err, "ModifyFunctionNoLock")
-		retcode = consts.ERROR_CODE__DB__READ
+		retcode = consts.ERROR_CODE__DB__UPDATE
 		return
 	}
 	return
@@ -69,7 +71,7 @@ func (t *Functions) InsertFunctionNoLock(o *orm.Ormer) (retcode int, err error) 
 	defer Logger.Info("[%v] left InsertFunctionNoLock.", t.Uri)
 	if o == nil {
 		err = errors.New("param `orm.Ormer` ptr empty")
-		retcode = consts.ERROR_CODE__PARAM__ILLEGAL
+		retcode = consts.ERROR_CODE__SOURCE_DATA__ILLEGAL
 		return
 	}
 	if _, err = (*o).Insert(t); err != nil {
@@ -101,24 +103,12 @@ func_id=12 invalid	......
  * 存储结构为Map存储
 */
 
-// 正则
-type RegTree struct {
-	Name     string
-	RegMatch *regexp.Regexp
-	Tree     *Tree
-	Value    int
-}
-
-type Leaf struct {
-	LeafFixMap    map[string]int // 非正则
-	LeafWildCards []*RegTree     // 正则
-}
-
-// 每个节点由三部分组成：1.非正则；2.正则；3.叶子
+// 每个节点由两部分组成：1.非正则；2.正则
 type Tree struct {
-	FixMap    map[string]*Tree // 非正则
-	WildCards []*RegTree       // 正则
-	Leaf      []*Leaf          // 叶子
+	Trees    []*Tree
+	RegMatch *regexp.Regexp
+	Name     string // 节点名称
+	Value    int    // 节点值, 也称功能ID
 }
 
 func (t *Functions) TableName() string {
@@ -126,7 +116,13 @@ func (t *Functions) TableName() string {
 }
 
 var (
-	RootMap map[string]*Tree
+	RootMap map[string]*Tree = map[string]*Tree{
+		"GET":    &Tree{Value: -1}, // -1: 头结点；0：中间节点； >0 ：叶子节点
+		"POST":   &Tree{Value: -1},
+		"PUT":    &Tree{Value: -1},
+		"PATCH":  &Tree{Value: -1},
+		"DELETE": &Tree{Value: -1},
+	}
 )
 
 // 获取构建树的数据源
@@ -143,8 +139,9 @@ func getAllFunctions() (funcs []Functions, err error) {
 	return
 }
 
-// 构建树
+// 构建多叉树
 func setupTree(root **Tree, levels []string, funcId int) (node **Tree) {
+	var index, length int = 0, 0
 	// 根节点为空，新建节点
 	if *root == nil {
 		*root = new(Tree)
@@ -157,96 +154,88 @@ func setupTree(root **Tree, levels []string, funcId int) (node **Tree) {
 	if len(levels) > 1 {
 		// 如果是冒号开头，则为正则表达式
 		if strings.HasPrefix(levels[0], ":") {
-			// ::TODO  如果uri中带有如 /v1/accounts/:id(\+d)/invalid
+			//  如果uri中带有如 /v1/accounts/:id(\+d)/invalid
 			// 例子中带有指定的正则表达式，则有替换、分割等操作，Name=:id ;   RegMatch=MustCompile("\+d")
-			if (*root).WildCards == nil {
-				(*root).WildCards = append((*root).WildCards,
-					&RegTree{
-						Name:     levels[0],
-						RegMatch: regexp.MustCompile("^.+$"),
-						Tree:     new(Tree),
-					})
-			} else {
-				index := 0
-				for index = 0; index < len((*root).WildCards); index++ {
-					if (*root).WildCards[len((*root).WildCards)-1].Name != levels[0] {
-						break
-					}
-				}
-				if index != len((*root).WildCards) {
-					(*root).WildCards = append((*root).WildCards,
-						&RegTree{
-							Name:     levels[0],
-							RegMatch: regexp.MustCompile("^.+$"),
-							Tree:     new(Tree),
-						})
+			for index = 0; (*root).Trees != nil && index < len((*root).Trees); index++ {
+				if (*root).Trees[index].Name == levels[0] {
+					break
 				}
 			}
-			len := len((*root).WildCards)
-			// 正则表达式节点的递归操作, 则levels减少一级
-			return setupTree(&((*root).WildCards[len-1].Tree), levels[1:], funcId)
+			// 同一个父亲的子节点中没有找到相同的正则表达式
+			if (*root).Trees == nil || index == len((*root).Trees) {
+				(*root).Trees = append((*root).Trees, &Tree{
+					RegMatch: regexp.MustCompile("^.+$"),
+					Name:     levels[0],
+				})
+				length = len((*root).Trees)
+				return setupTree(&((*root).Trees[length-1]), levels[1:], funcId)
+			} else {
+				// 正则表达式节点的递归操作, 则levels减少一级
+				return setupTree(&((*root).Trees[index]), levels[1:], funcId)
+			}
 		} else {
 			// 否则，为非正则表达式
-			if (*root).FixMap == nil {
-				(*root).FixMap = map[string]*Tree{
-					levels[0]: new(Tree),
+			for index = 0; (*root).Trees != nil && index < len((*root).Trees); index++ {
+				if (*root).Trees[index].Name == levels[0] {
+					break
 				}
-			} else if _, ok := (*root).FixMap[levels[0]]; !ok {
-				(*root).FixMap[levels[0]] = new(Tree)
 			}
-			tempTree := (*root).FixMap[levels[0]]
-			// 非正则表达式节点的递归操作, 则levels减少一级
-			return setupTree(&tempTree, levels[1:], funcId)
+			if (*root).Trees == nil || index == len((*root).Trees) {
+				(*root).Trees = append((*root).Trees, &Tree{
+					Name: levels[0],
+				})
+				length = len((*root).Trees)
+				return setupTree(&((*root).Trees[length-1]), levels[1:], funcId)
+			} else {
+				// 非正则表达式节点的递归操作, 则levels减少一级
+				return setupTree(&((*root).Trees[index]), levels[1:], funcId)
+			}
 		}
 	} else {
 		// 如果是叶子节点
 		// 如果是冒号开头，则为正则表达式
-		(*root).Leaf = append((*root).Leaf, new(Leaf))
-		len := len((*root).Leaf)
 		if strings.HasPrefix(levels[0], ":") {
 			// ::TODO  如果uri中带有如 /v1/accounts/:id(\+d)/invalid
 			// 例子中带有指定的正则表达式，则有替换、分割等操作，Name=:id ;   RegMatch=MustCompile("\+d")
-			(*root).Leaf[len-1].LeafWildCards = append((*root).Leaf[len-1].LeafWildCards,
-				&RegTree{
-					Name:     levels[0],
-					RegMatch: regexp.MustCompile("^.+$"),
-					Tree:     new(Tree),
-					Value:    funcId,
-				})
+			(*root).Trees = append((*root).Trees, &Tree{
+				RegMatch: regexp.MustCompile("^.+$"),
+				Name:     levels[0],
+				Value:    funcId,
+			})
 		} else {
 			// 否则，为非正则表达式
-			(*root).Leaf[len-1].LeafFixMap = map[string]int{
-				levels[0]: funcId,
-			}
+			(*root).Trees = append((*root).Trees, &Tree{
+				Name:  levels[0],
+				Value: funcId,
+			})
 		}
 	}
 	return root // 叶子节点
 }
 
-// 初始化功能树
+// 初始化多叉树, 并把func表数据加载到内存中
 func LoadMapTree() (err error) {
-	var (
-		funcs []Functions
-	)
-	RootMap = map[string]*Tree{}
-	funcs, err = getAllFunctions()
-	if err != nil {
-		Logger.Error(err.Error())
-		return
-	}
-	RootMap["GET"] = &Tree{}
-	RootMap["POST"] = &Tree{}
-	RootMap["PUT"] = &Tree{}
-	RootMap["PATCH"] = &Tree{}
-	RootMap["DELETE"] = &Tree{}
 	var (
 		tree1 *Tree = RootMap["GET"]
 		tree2 *Tree = RootMap["POST"]
 		tree3 *Tree = RootMap["PUT"]
 		tree4 *Tree = RootMap["PATCH"]
 		tree5 *Tree = RootMap["DELETE"]
+
+		funcs []Functions
 	)
+	if funcs, err = getAllFunctions(); err != nil {
+		Logger.Error(err.Error())
+		return
+	}
+	fmt.Printf("len(func)=%d\n", len(funcs))
+
 	for index := 0; funcs != nil && index < len(funcs); index++ {
+		funcs[index].Uri = strings.TrimSpace(funcs[index].Uri)
+		if funcs[index].Uri == "" || !strings.HasPrefix(funcs[index].Uri, "/") {
+			fmt.Printf("databases funcs uri `[%s]` not irregular, funcId=%d\n", funcs[index].Uri, funcs[index].Id)
+			return
+		}
 		switch int(funcs[index].MethodType) {
 		case METHOD_TYPE_GET:
 			setupTree(&tree1, strings.Split(funcs[index].Uri, "/")[1:], funcs[index].Id)
@@ -315,29 +304,15 @@ func printTree(tree *Tree, stack *Stack) {
 	if tree == nil {
 		return
 	}
-	if tree.Leaf != nil {
-		for index := 0; index < len(tree.Leaf); index++ {
-			for key, value := range tree.Leaf[index].LeafFixMap {
-				stack.PrintDeTree(key, "/", value)
-			}
-			if len(tree.Leaf[index].LeafWildCards) > 0 {
-				stack.PrintDeTree(tree.Leaf[index].LeafWildCards[0].Name, "/", tree.Leaf[index].LeafWildCards[0].Value)
-			}
-		}
+	if tree.Value > 0 {
+		stack.PrintDeTree(tree.Name, "/", tree.Value)
 	}
-	if tree.FixMap != nil {
-		for key, value := range tree.FixMap {
-			stack.Push(key)
-			printTree(value, stack)
-		}
+	for index := 0; tree.Trees != nil && index < len(tree.Trees); index++ {
+		stack.Push(tree.Name)
+		printTree(tree.Trees[index], stack)
+		stack.Pop()
 	}
-	if tree.WildCards != nil && len(tree.WildCards) > 0 {
-		for index := 0; index < len(tree.WildCards); index++ {
-			stack.Push(tree.WildCards[index].Name)
-			printTree(tree.WildCards[index].Tree, stack)
-		}
-	}
-	stack.Pop()
+	return
 }
 
 // 打印树
@@ -358,83 +333,38 @@ type HttpRequestInfo struct {
 	Uri    string `json:"uri"`
 }
 
-func getFuncId(fields []string, method string) (funcId int, entityStr string, retcode int, err error) {
-	var (
-		subIndex int  = 0
-		first    bool = false // 当获取到第一个实体ID后，直接把实体ID赋值给entityStr，以后再遇到不用再赋值了，因为后续的实体ID是前一个实体ID的子资源，粒度比较小，不考虑
-		// 如：/v1/wechats/:id/pay/jsapi_params/:bill_id/open_id/:open_id
-		// 则 :id 为唯一资源标识符，不用考虑二级、三级等子粒度资源
-	)
-	tree := RootMap[method]
-	fmt.Println("fields: ", fields)
-	for index := 0; index < len(fields); index++ {
-		// 叶子节点
-		if index == len(fields)-1 {
-			if tree.Leaf == nil {
-				// 1.匹配不到
-				return -1, "", 0, nil
+func recursiveMatchNode(trees []*Tree, fields []string) (funcId int, entityIdStr string) {
+	var index = 0
+	if trees == nil || len(trees) <= 0 || fields == nil || len(fields) <= 0 {
+		return
+	}
+	for index = 0; index < len(trees); index++ {
+		if trees[index].RegMatch == nil && trees[index].Name == fields[0] {
+			if trees[index].Value > 0 && len(fields) == 1 {
+				fmt.Printf("value=%d\n", trees[index].Value)
+				funcId = trees[index].Value
+				return
 			}
-			// 优先匹配非正则表达式
-			for subIndex = 0; subIndex < len(tree.Leaf); subIndex++ {
-				if _, ok := tree.Leaf[subIndex].LeafFixMap[fields[index]]; !ok {
-					continue
-				} else {
-					break
+			return recursiveMatchNode(trees[index].Trees, fields[1:])
+		}
+		if trees[index].RegMatch != nil && trees[index].RegMatch.MatchString(fields[0]) {
+			if _, err := strconv.ParseInt(fields[0], 10, 64); err == nil {
+				fmt.Printf("通配符WildCard=%s\n", trees[index].Name)
+				entityIdStr = fields[0]
+				if trees[index].Value > 0 && len(fields) == 1 {
+					funcId = trees[index].Value
+					return
 				}
-			}
-			// 已匹配到
-			if subIndex != len(tree.Leaf) {
-				return tree.Leaf[subIndex].LeafFixMap[fields[index]], entityStr, 0, nil
-			}
-			// 否则，匹配正则表达式
-			for subIndex = 0; subIndex < len(tree.Leaf); subIndex++ {
-				for _, wildcard := range tree.Leaf[subIndex].LeafWildCards {
-					// 匹配成功
-					if wildcard.RegMatch.MatchString(fields[index]) {
-						if entityStr == "" {
-							entityStr = fields[index]
-						}
-						return wildcard.Value, entityStr, 0, nil
-					}
-				}
-			}
-			// 没有匹配到
-			if subIndex == len(tree.Leaf) {
-				return -1, "", 0, nil
-			}
-		} else {
-			// 非叶子节点
-			// 优先匹配非正则表达式
-			if _, ok := tree.FixMap[fields[index]]; ok {
-				// 匹配到中间节点
-				tree = tree.FixMap[fields[index]]
-				continue
-			}
-			if tree.WildCards == nil {
-				// 没有匹配到
-				return -1, "", 0, nil
-			}
-			// flag 标志位， 如果成功正则匹配，直接goto跳转到最开始的循环处
-			// 否则，没有匹配到，则退出
-			flag := false
-			for subIndex = 0; subIndex < len(tree.WildCards); subIndex++ {
-				if tree.WildCards[subIndex].RegMatch.MatchString(fields[index]) {
-					if !first {
-						entityStr = fields[index]
-						first = true
-					}
-					tree = tree.WildCards[subIndex].Tree
-					flag = true
-					break
-				}
-			}
-			if !flag && subIndex == len(tree.WildCards) {
-				// 没有匹配到
-				return -1, "", 0, nil
+				fmt.Printf("value=%d, name=%s\n", trees[index].Value, trees[index].Name)
+				return recursiveMatchNode(trees[index].Trees, fields[1:])
 			}
 		}
 	}
-	return -1, "", 0, nil
+	if index == len(trees) {
+		// 没有匹配到
+		return -1, ""
+	}
+	return
 }
 
 func GetFuncId(info *HttpRequestInfo) (funcId int, entityStr string, retcode int, err error) {
@@ -452,10 +382,8 @@ func GetFuncId(info *HttpRequestInfo) (funcId int, entityStr string, retcode int
 		uri.Path = uri.Path[0 : len(uri.Path)-1]
 	}
 	// 解析path，在解析树中验证一把path，看是否能取出功能ID
-	if funcId, entityStr, retcode, err = getFuncId(strings.Split(uri.Path, "/")[1:], info.Method); err != nil {
-		err = errors.Wrap(err, "GetFuncId")
-		return
-	}
+	fmt.Println("path: ", uri.Path)
+	funcId, entityStr = recursiveMatchNode(RootMap[info.Method].Trees, strings.Split(uri.Path, "/")[1:])
 	// 1.1 获取实体ID最快的做法是，在解析查找功能ID的过程中，直接把实体ID获取到
 
 	// 1.2 说明：本来为了代码质量和可读性，获取实体ID的做法
@@ -556,7 +484,7 @@ func GetFunctions(pageIndex int64, pageSize int64, searchKey string) (funcInfos 
 func GetThirdReginMarkKey(uri string) (markKey string, retcode int, err error) {
 	if strings.TrimSpace(uri) == "" && strings.HasPrefix(uri, "/") {
 		err = errors.New("param `uri` can't be empty , and prefix '/'")
-		retcode = consts.ERROR_CODE__PARAM__ILLEGAL
+		retcode = consts.ERROR_CODE__SOURCE_DATA__ILLEGAL
 		return
 	}
 	fields := strings.Split(uri, "/")
